b75adeef2d0afc9cc196010d679c5af3d7fe3645
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-10-20 15:39:29 +0700

    initial commit

---
+++ app/views/index.php
@@ -0,0 +1,10 @@
+<?php
+// This is how you echo out database information on the screen
+foreach ($data['users'] as $user) {
+    echo "Information: " . $user->user_name . " " . $user->user_email;
+    echo "<br>";
+}
+
+// echo $data['title'];
+?>
+<!-- HOME PAGE -->
